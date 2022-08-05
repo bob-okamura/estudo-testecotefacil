12ac99d9c3563f7dd8d321aa377dc1d4a8de08fd
Roberto Okamura <rob.okamura@gmail.com>
2022-08-05 19:50:10 -0300

    All 14 tests passing

--- src/test/java/br/com/bobhome/controllers/PlanetControllerIT.java
+++ src/test/java/br/com/bobhome/controllers/PlanetControllerIT.java
@@ -1,5 +1,6 @@
 package br.com.bobhome.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,11 +33,15 @@ public class PlanetControllerIT { //IT Integration Test
 	
 	private PlanetDTO planetDTO;
 	private Long countTotalPlanets;
+	private Long existingId;
+	private Long nonexistingId;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		planetDTO = Factory.createPlanetDTO();
 		countTotalPlanets = 5L;
+		existingId = 1L;
+		nonexistingId = 1000L;
 	}
 	
 	@Test
@@ -47,12 +52,19 @@ public class PlanetControllerIT { //IT Integration Test
 				.content(jsonBody)
 				.contentType(MediaType.APPLICATION_JSON)
 			.accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isCreated())
-		;
+		.andExpect(status().isCreated());
 	}
 	
 	@Test
-	public void findAllShouldReturnAllResourcesPaged() throws Exception {
+	public void findByNameShouldReturnName() throws Exception {
+		mockMvc.perform(get("/planets/names?name=ho")
+					.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+		
+	}
+	
+	@Test
+	public void findAllShouldReturnPage() throws Exception {
 		mockMvc.perform(get("/planets")
 					.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk())
@@ -61,6 +73,38 @@ public class PlanetControllerIT { //IT Integration Test
 		.andExpect(jsonPath("$.content[0].name").value("Alderaan"))
 		.andExpect(jsonPath("$.content[1].name").value("Hoth"))
 		.andExpect(jsonPath("$.content[2].name").value("Dagobah"));
+	}
+	
+	@Test
+	public void findByIdShouldReturnPlanetWhenIdExists() throws Exception {
+		mockMvc.perform(get("/planets/{id}", existingId)
+					.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.id").exists())
+		.andExpect(jsonPath("$.name").exists())
+		.andExpect(jsonPath("$.climate").exists())
+		.andExpect(jsonPath("$.terrain").exists());;
+	}
+	
+	@Test
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+		mockMvc.perform(get("/planets/{id}", nonexistingId)
+					.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void deleteShouldReturnNoContentWhenIdExists() throws Exception {
+		mockMvc.perform(delete("/planets/{id}", existingId)
+					.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void deleteShoundReturnNotFoundWhenIdDoesNotExist() throws Exception {
+		mockMvc.perform(delete("/planets/{id}", nonexistingId)
+					.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
 	}
 	
 }
--- src/test/java/br/com/bobhome/services/PlanetServiceIT.java
+++ src/test/java/br/com/bobhome/services/PlanetServiceIT.java
@@ -7,28 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.bobhome.dtos.PlanetDTO;
+import br.com.bobhome.repositories.PlanetRepositoty;
 import br.com.bobhome.services.exceptions.ResourceNotFoundException;
+import br.com.bobhome.tests.Factory;
 
 @SpringBootTest
 @Transactional
 public class PlanetServiceIT { // IT Integration Test
 	
+	@Autowired
+	private PlanetService service;
+	
+	@Autowired
+	private PlanetRepositoty repository;
+	
 	private Long countTotalPlanets;
 	private Long existingId;
 	private Long nonExistingId;
-	
+	private PlanetDTO planetDTO;
+		
 	@BeforeEach
 	void setUp() throws Exception {
 		countTotalPlanets = 5L;
 		existingId = 1L;
 		nonExistingId = 1000L;
+		planetDTO = Factory.createPlanetDTO();
 	}
-
-	@Autowired
-	private PlanetService service;
+	
+	@Test
+	public void insertShouldInsertData() {
+		service.insert(planetDTO);
+		
+		Assertions.assertTrue(true);
+	}
 
 	@Test
 	public void findAllPagedShouldReturnPageWhenPage0Size10() {
@@ -41,7 +56,19 @@ public class PlanetServiceIT { // IT Integration Test
 		Assertions.assertEquals(10, result.getSize());
 		Assertions.assertEquals(countTotalPlanets, result.getTotalElements());
 	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
 
+		Page<PlanetDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Alderaan", result.getContent().get(0).getName());
+		Assertions.assertEquals("Bespin", result.getContent().get(1).getName());
+		Assertions.assertEquals("Dagobah", result.getContent().get(2).getName());
+	}
+	
 	@Test
 	public void findByIdShouldReturnPlanetWhenIdExists() {
 
@@ -55,6 +82,21 @@ public class PlanetServiceIT { // IT Integration Test
 	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.findById(nonExistingId);
+		});
+	}
+	
+	@Test
+	public void deleteShouldDeleteWhenIdExists() {
+		
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalPlanets - 1, repository.count());
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
 		});
 	}
 
